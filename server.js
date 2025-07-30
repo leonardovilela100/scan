0182084027725166f6410bb210c734f61a7f1dea
Leonardo Carvalho Vilela <leovilela100@gmail.com>
2025-07-30 15:13:18 -0300

    first commit

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "8442922985:AAFNcmR2yqERaoVf28aENwewRrOpwEBfU1U";
+const TELEGRAM_CHAT_ID = "5693440005";
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
